83e6205def5bf78ac5a9e0dfbab113a94140ce39
JJ Brown <jack.brown.255@gmail.com>
2022-02-08 23:54:55 -0600

    Added utilities to write an .ips file or to apply its changes to a ROM.

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,9 @@
 
 use std::borrow::Cow;
 use std::fmt;
+use std::fs::File;
+use std::io::{Seek, Write};
+use std::io::SeekFrom;
 
 use byteorder::BigEndian;
 use byteorder::ByteOrder;
@@ -63,6 +66,53 @@ impl<'a> Patch<'a> {
     pub fn truncation(&self) -> Option<usize> {
         self.truncation
     }
+
+    pub fn ips_file_bytes(&self) -> Vec<u8> {
+        let mut res: Vec<u8> = Vec::new();
+
+        push_all(&mut res, b"PATCH");
+
+        for hunk in &self.hunks {
+            let mut hunk_bytes = hunk.ips_file_bytes();
+            res.append(&mut hunk_bytes);
+        }
+
+        push_all(&mut res, b"EOF");
+
+        return res;
+    }
+
+    pub fn apply_to_rom(&self, rom_data: &[u8]) -> std::result::Result<Vec<u8>, Error> {
+        let mut new_rom_data = Vec::from(rom_data);
+
+        for hunk in self.hunks.iter() {
+            // checking this explicitly to avoid panicking when we call .copy_from_slice()
+            if hunk.offset + hunk.payload.len() > new_rom_data.len() {
+                return Err(Error("Patch data in hunk extended past the end of the ROM".to_string()));
+            }
+
+            new_rom_data[hunk.offset..hunk.offset + hunk.payload.len()]
+                .copy_from_slice(hunk.payload())
+        }
+
+        if let Some(truncation) = self.truncation() {
+            new_rom_data.truncate(truncation)
+        }
+
+        Ok(new_rom_data)
+    }
+
+    pub fn write_to_rom(&self, rom_file: &mut File) -> std::io::Result<()> {
+        for hunk in self.hunks.iter() {
+            hunk.write_to_rom(rom_file)?
+        }
+
+        if let Some(truncation) = self.truncation() {
+            rom_file.set_len(truncation as u64)?;
+        }
+
+        return std::io::Result::Ok(());
+    }
 }
 
 /// IPS parsing error.
@@ -93,6 +143,38 @@ impl<'a> Hunk<'a> {
     /// The data that should be overwritten at the offset.
     pub fn payload(&self) -> &[u8] {
         &self.payload
+    }
+
+    // returns the starting offset of this hunk as a 2-byte big-endian number
+    fn offset_bytes(&self) -> [u8; 3] {
+        let mut buf: [u8; 3] = [0; 3];
+        BigEndian::write_u24(&mut buf, self.offset as u32);
+        return buf;
+    }
+
+    // returns the length of this hunk as a 2-byte big-endian number
+    fn length_bytes(&self) -> [u8; 2] {
+        let mut buf: [u8; 2] = [0; 2];
+        BigEndian::write_u16(&mut buf, self.payload.len() as u16);
+        return buf;
+    }
+
+    fn ips_file_bytes(&self) -> Vec<u8> {
+        let mut res: Vec<u8> = Vec::new();
+        res.reserve_exact(3 + 2 + self.payload.len());
+
+        push_all(&mut res, &self.offset_bytes());
+        push_all(&mut res, &self.length_bytes());
+        push_all(&mut res, &*self.payload);
+
+        return res;
+    }
+
+    fn write_to_rom<T: Seek + Write>(&self, rom_file: &mut T) -> std::io::Result<()> {
+        rom_file.seek(SeekFrom::Start(self.offset() as u64))?;
+        rom_file.write_all(self.payload())?;
+
+        return std::io::Result::Ok(());
     }
 }
 
@@ -131,5 +213,11 @@ fn be_int(len: usize) -> impl Fn(&[u8]) -> IResult<&[u8], usize> {
     move |input: &[u8]| {
         let (input, bytes) = take(len)(input)?;
         Ok((input, BigEndian::read_uint(bytes, len) as usize))
+    }
+}
+
+fn push_all(vec: &mut Vec<u8>, data: &[u8]) {
+    for byte in data {
+        vec.push(*byte)
     }
 }
